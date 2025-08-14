c644a5bd3679411d39fa469180a84e1226c6c75e
Your Name <you@example.com>
2025-08-14 13:34:11 +0530

    integrated redis cache

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-import 'dotenv/config';
-import express, { json } from 'express';
-import { authenticate, sync } from './models/index.js';
-import authRoutes from './routes/authRoutes.js';
-import movieRoutes from './routes/movieRoutes.js';
-import reservationRoutes from './routes/reservationRoutes.js';
+import "dotenv/config";
+import "./utils/redisClient.js";
+import express, { json } from "express";
+import sequelize from "./models/index.js";
+import authRoutes from "./routes/authRoutes.js";
+import movieRoutes from "./routes/movieRoutes.js";
+import reservationRoutes from "./routes/reservationRoute.js";
 // esure redis is connected
-import './utils/redisClient.js';
 
 const PORT = process.env.PORT || 3000;
 
@@ -14,18 +14,18 @@ const app = express();
 app.use(json());
 
 // Routes
-app.use('/auth', authRoutes);
-app.use('/movies', movieRoutes);
-app.use('/reservations', reservationRoutes);
+app.use("/auth", authRoutes);
+app.use("/movies", movieRoutes);
+app.use("/reservations", reservationRoutes);
 
 // Start the server
 app.listen(PORT, async () => {
   console.log(`Server is running on port ${PORT}`);
 
   try {
-    await authenticate();
-    await sync();
+    await sequelize.authenticate();
+    await sequelize.sync({ alter: true });
   } catch (error) {
-    console.error('Unable to connect to the database:', error);
+    console.error("Unable to connect to the database:", error);
   }
 });
